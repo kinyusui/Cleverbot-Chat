88dce83ac616a06d93a521544a3c2b0f30c9be1f
kinyusui <kaixngchen@gmail.com>
2017-09-16 15:05:48 -0400

    almost working and complete frontend just need to make cleverbot replies update

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
- 
+import axios from 'axios';
 /**
  * A counter button: tap the button to increase the count.
  */
@@ -7,15 +7,43 @@ class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      reply: '',
+      reply: 'no reply yet',
+      message: ''
     };
+    this.handlemessage.bind(this);
+    this.onSubmit.bind(this);
   }
- 
+
+  handlemessage (event) {
+    this.setState({
+      message: event.target.value
+    });
+  }
+  onSubmit (event) {
+    event.preventDefault();
+    var obj = {message: this.state.message}
+    axios.post('/ChatBot',obj)
+    .then(function(result) {
+      console.log(`posted my dude`, result);
+      axios.get('/ChatBot')
+      .then(function(result) {
+        var data = result.data;
+        console.log(`this is the get`, data[data.length-1][1].substring(10));
+        this.setState({
+          reply: data[data.length-1][1].substring(10)
+        })
+      })
+    })
+  }
   render() {
     return (
-      <form action="/ChatBot">
-        User: <input type="text" value=""/>
-      </form>
+      <div className='outer'> 
+        <div className='cleverbot'>Cleverbot Replies: {this.state.reply}</div>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          User says: <input type="text" name="message" value={this.state.message} onChange={this.handlemessage.bind(this)}/>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
     );
   }
 }
